85ac6a07efe058db51f2eae2a76825f40c06d828
Your Name <marinabananas@gmail.com>
2022-07-15 01:38:08 +0300

    quiz

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ class Student:
         self.grades = {}
 
     def add_courses(self, course_name):
-        self.finished_course.append(course_name)
+        self.finished_courses.append(course_name)
+
+    def rate_cl(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Mentor:
@@ -17,6 +26,13 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __int__(self, name, surname):
+        self.grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
