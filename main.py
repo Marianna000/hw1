c9b723302edcd537c02252b88c140f0415566f98
Your Name <marinabananas@gmail.com>
2022-08-02 18:42:00 +0300

    quiz

--- main.py
+++ main.py
@@ -7,20 +7,12 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def rate_cl(self, lecturer, course, grade):
-        if isinstance(lecturer,
-                      Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
-            if course in lecturer.grades:
-                lecturer.grades[course] += [grade]
-            else:
-                lecturer.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
     def __str__(self):
         return print(f"Студенты: Имя:{self.name}, Фамилия:{self.surname}, Пол:{self.gender}, "
                      f"Средняя оценка за домашние задания:, Курсы в процессе изучения: {self.courses_in_progress}, "
                      f"Завершенные курсы: {self.finished_courses} ")
+
+    #расчёт средней оценки написать функцию
 
 
 class Mentor:
@@ -51,6 +43,13 @@ class Reviewer(Mentor):
             else:
                 student.grades[course] = [grade]
         else:
+            return 'Ошибка' def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
             return 'Ошибка'
 
     def __str__(self):
@@ -59,18 +58,20 @@ class Reviewer(Mentor):
 
 # проверка
 
-# if __name__ == "__main__":
+# студенты:
 student1 = Student("Alla", "Pugacheva", "women")
 student1.courses_in_progress += ['Python']
 student1.finished_courses += ['Java']
-student2 = Student("Igor", "Rurikovich", "men")
 student1.__str__()
+
+student2 = Student("Igor", "Rurikovich", "men")
 student2.__str__()
 
-
+#проверяющие:
 reviewer1 = Reviewer("Van", "Helsing")
 reviewer1.__str__()
 
+#лекторы:
 lecturer1 = Lecturer("Macheha", "Zlaya")
 lecturer1.__str__()
 
