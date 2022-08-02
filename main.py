c19a828681ef57e2085723c25fcdd2c524ec5787
Your Name <marinabananas@gmail.com>
2022-08-02 18:07:17 +0300

    quiz

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ class Student:
         list_avg = list_sum / len(self.grades)
 
     def rate_cl(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -24,7 +25,7 @@ class Student:
             return 'Ошибка'
 
     def __str__(self):
-        return print(self.name, self.surname, round(self._class_ar(),2), ''.join(self.courses_in_progress),''.join(self.finished_courses))
+        return print(f"Студенты: Имя:{self.name}, Фамилия:{self.surname}, Пол:{self.gender}")
 
 
 class Mentor:
@@ -35,15 +36,16 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    def __int__(self, name, surname):
-        self.grades = {}
+    # def __init__(self, name, surname):
+    #     self.grades = {}
 
     def _class_ar(self):
         list_sum = sum(self.grades)
-        list_avg = list_sum/len(self.grades)
+        list_avg = list_sum / len(self.grades)
 
     def __str__(self):
-        return print(self.name, self.surname, round(self._class_ar(),2))
+        return print(f"Лекторы: Имя:{self.name}, Фамилия:{self.surname}")
+        # .self.name, self.surname,round(self._class_ar(), 2)
 
 
 class Reviewer(Mentor):
@@ -57,7 +59,25 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
     def __str__(self):
-        return print(self.name, self.surname)
+        return print(f"Проверяющие: Имя:{self.name}, Фамилия:{self.surname}")
 
-#
+
+# проверка
+
+# if __name__ == "__main__":
+student1 = Student("Alla", "Pugacheva", "women")
+student2 = Student("Igor", "Rurikovich", "men")
+student1.__str__()
+student2.__str__()
+
+reviewer1 = Reviewer("Van", "Helsing")
+reviewer1.__str__()
+
+lecturer1 = Lecturer("Macheha", "Zlaya")
+lecturer1.__str__()
+
+
+
+
+
 
